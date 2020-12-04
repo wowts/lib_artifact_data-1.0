48ccd05931b2d9e1dccf04b7d815fda8f30ac8cc
Sidoine De Wispelaere <sidoine@sidoine.net>
2020-12-04 13:35:18 +0100

    Add lua section

--- index.d.ts
+++
@@ -1,6 +0,0 @@
-import { LuaArray } from "@wowts/lua";
-export declare function RegisterCallback(arg0: any, arg1: any, arg2: any): void;
-export declare function UnregisterCallback(arg0: any, arg1: any): void;
-export declare function GetArtifactTraits(): [string, LuaArray<{
-    spellID: string;
-}>];
